bc5548f4805f6134f46b5e1c59d9e9c4e9d8a4fa
erick16-max <erickgege16@gmail.com>
2022-02-16 17:15:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gegerick portfolio
+ A personal static portfolio using pure CSS and HTML with a highly responsive aspect using flexboxes. The portfolio uses CSS, HTML & FontAwesome icons in displaying the static website.
---
+++ script.js
@@ -0,0 +1,29 @@
+const toggleButton = document.getElementsByClassName('navbar-toggle')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links');
+
+toggleButton.addEventListener('click' , function () {
+    for (let i=0; i< navbarLinks.length ; i++) {
+        navbarLinks[i].classList.toggle('active');
+    }
+});  
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
