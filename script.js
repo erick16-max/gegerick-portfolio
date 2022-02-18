35f411baec5f29d95fb40e10b88cfc7ae53fadf7
erick16-max <erickgege16@gmail.com>
2022-02-18 11:26:04 +0300

    updating portfolio

--- script.js
+++ script.js
@@ -8,22 +8,14 @@ toggleButton.addEventListener('click' , function () {
 });  
 
 
-//Get the button
-var mybutton = document.getElementById("myBtn");
+//btn scroll to the top
+const btnScrollToP = document.querySelector('#btn-top');
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+btnScrollToP.addEventListener('click', function (){
+  
+  window.scrollTo({
+    top:0,
+    left:0,
+    behavior: "smooth"
+  });
+});
